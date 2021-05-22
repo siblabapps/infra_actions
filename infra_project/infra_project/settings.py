e2a24d9cd354533b02f3ffef6794ba6321390ec5
siblabapps <siblabapps@gmail.com>
2021-05-23 00:57:49 +0700

    deploy6

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -7,7 +7,11 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "84.201.161.40:5000"
+    "localhost",
+    "127.0.0.1",
+    "84.201.161.40",
+    "[::1]",
+    "testserver",
 ]
 
 INSTALLED_APPS = [
